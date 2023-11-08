759b713ff93f10e4cd4dafe701378daf17a448da
dimaskuntoaji <kuntoajidimas@gmail.com>
2023-11-08 19:17:38 +0700

    upload-project

---
+++ app/db/seeders/20231101123458-seeder-user.js
@@ -0,0 +1,33 @@
+'use strict';
+const bcrypt = require('bcryptjs');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const password = bcrypt.hashSync('rahasia', 10);
+      await queryInterface.bulkInsert(
+        "Users",
+        [
+          {
+            name: "John Doe",
+            email: "admin@gmail.com",
+            password: password,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            name: "Doe",
+            email: "doe@gmail.com",
+            password: password,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+        ],
+        {}
+      );
+  },
+
+  async down (queryInterface, Sequelize) {
+      await queryInterface.bulkDelete('Users', null, {});
+  }
+};
